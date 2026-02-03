1d706d2e96aa452bfcd05d3e045b7291161b3691
Vickiii <au792000@uni.au.dk>
2026-02-03 15:24:52 +0100

    hej

--- ShopOrg/ShoeShop.h
+++ ShopOrg/ShoeShop.h
@@ -1,17 +1,35 @@
+
 #ifndef SHOESHOP_H
 #define SHOESHOP_H
 
+/*
 class ShoeShop {
 private:
 	int* shelf;
 	int size;
 public:
-	ShoeShop(int);
-	~ShoeShop();
-	void add_items(int, int);
-	int in_stock(int);
-	int count_items();
-	bool clear(int);
+	ShoeShop(int); <-- constructor
+	~ShoeShop(); <-- destructor 
+	void add_items(int, int); <-- the function adds shoes to the shop
+	int in_stock(int); <-- checks if the shoes is in stock
+	int count_items(); <-- it counts amount of items
+	bool clear(int); <-- removes specific items or clears shelfs
+};
+*/
+
+//This is our solution for question 1:
+#include <stdio.h>
+
+struct Shoeshop {
+	int* shelf;
+	int size;
 };
 
+void add_items(int , int ){
+	
+}
+
 #endif
+
+
+
