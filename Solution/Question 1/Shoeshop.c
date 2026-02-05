49182d43f7c17460caede6d01bf198277b7d86f6
Seyn0012 <au800162@uni.au.dk>
2026-02-05 15:58:48 +0100

    jjj

--- Solution/Question 1/Shoeshop.c
+++ Solution/Question 1/Shoeshop.c
@@ -23,8 +23,33 @@ void free_ShoeShop(ShoeShop* shop) {
     free(shop->shelf);
     free(shop);
     }
-
 }  
+
+//Function that adds items to the shop
+void ShoeShop_add_items(ShoeShop* shop, int row, int num){
+    //Adds num items to the specified row in the shop (shop is a pointer to ShoeShop)
+    shop->shelf[row] += num;
+}
+
+//Function that checks if the item is in stock
+int ShoeShop_in_stock(ShoeShop* shop, int row){
+    return shop->shelf[row] > 0;
+}
+
+//Function that counts the total items in the shop
+int ShoeShop_count_items(ShoeShop* shop){
+    int tot = 0;
+    for(int k = 0; k < shop->size; k++){
+        tot += shop->shelf[k];
+    }
+    return tot;
+}
+
+bool ShoeShop_clear(ShoeShop* shop, int row){
+    bool rem = shop->shelf[row] > 0;
+    shop->shelf[row] = 0;
+    return rem;
+}
 
 
 
