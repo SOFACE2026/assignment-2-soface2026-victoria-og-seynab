2fc041c985af1323b7d37d804dfc460da2262eae
Seyn0012 <au800162@uni.au.dk>
2026-02-05 15:47:53 +0100

    ...

--- Solution/Question 1/Shoeshop.c
+++ Solution/Question 1/Shoeshop.c
@@ -1,3 +1,32 @@
 //This is our sulotion executed
 
 #include "Shoeshop2.h"
+#include <stdlib.h>
+
+//Constructor implemention
+ShoeShop* new_ShoeShop(int size) {
+//Allocates memory for the shop and initializes the shelves
+    ShoeShop* shop = (ShoeShop*)malloc(sizeof(ShoeShop));
+// Allocates memory to the array (new_int[size])
+    shop->shelf = (int*)malloc(size * sizeof(int));
+    shop->size = size;
+//Intializes the array elements to zero
+    for (int i = 0; i < size; i++) {
+        shop->shelf[i] = 0;
+    }
+    return shop;
+}
+
+//Destructor implementation
+void free_ShoeShop(ShoeShop* shop) {
+    if (shop == NULL) {
+    free(shop->shelf);
+    free(shop);
+    }
+
+}  
+
+
+
+
+
