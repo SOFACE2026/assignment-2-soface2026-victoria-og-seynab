0e79b25f2338755b8e159ee81a9a4a75c250c1b3
Vickiii <au792000@uni.au.dk>
2026-02-05 15:45:54 +0100

    done

---
+++ Solution/Question 2/ShoeAbs.h
@@ -0,0 +1,11 @@
+// Implement the code of ShopAbs in C emulating the polymorphism 
+// provided by means of the abstract class Shop. 
+// In this implementation function pointers are required. 
+// They need to be implemented as members of the struct Shop. 
+// The implementation of ShoeShop must declare the function
+// pointers in exactly the same order. 
+// A type cast to ’(Shop*)’ needs to be used to use a ShoeShop via a Shop pointer.
+
+
+
+
